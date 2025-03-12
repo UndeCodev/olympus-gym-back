c75b25cbd08fa57c5de0fcd3dd9ba22db78d9103
Diego Vite Hernández <undecode0@hotmail.com>
2025-03-12 01:27:39 -0600

    feat(routes): authentication middleware added in configuration routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 
 import configRoutes from '../modules/configuration/routes';
 import authRoutes from '../modules/auth/routes';
+import { authenticateMiddleware } from '../middlewares/authenticate';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/config', configRoutes);
+router.use('/config', authenticateMiddleware, configRoutes);
 
 export default router;
